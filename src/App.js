49a55b2a3388a63f31730f5d7b98c767a761c039
yashdadhich <98592915+yashdadhich@users.noreply.github.com>
2023-09-16 17:12:47 +0530

    code updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./data";
+import { ListContainer, ListItem } from "./styles";
+import { DragHandle } from "./partials/DragHandle";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 
-function App() {
+const App = () => {
+  const list = List.getList();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DragDropContext
+        onDragEnd={(param) => {
+          const srcI = param.source.index;
+          const desI = param.destination?.index;
+          if (desI) {
+            list.splice(desI, 0, list.splice(srcI, 1)[0]);
+            List.saveList(list);
+          } 
+        }}
+      >
+        <ListContainer>
+          <h1>The List</h1>
+          <Droppable droppableId="droppable-1">
+            {(provided, _) => (
+              <div ref={provided.innerRef} {...provided.droppableProps}>
+                {list.map((item, i) => (
+                  <Draggable
+                    key={item.id}
+                    draggableId={"draggable-" + item.id}
+                    index={i}
+                  >
+                    {(provided, snapshot) => (
+                      <ListItem
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        style={{
+                          ...provided.draggableProps.style,
+                          boxShadow: snapshot.isDragging
+                            ? "0 0 .4rem #666"
+                            : "none",
+                        }}
+                      >
+                        <DragHandle {...provided.dragHandleProps} />
+                        <span>{item.title}</span>
+                      </ListItem>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </ListContainer>
+      </DragDropContext>
     </div>
   );
-}
+};
 
 export default App;
